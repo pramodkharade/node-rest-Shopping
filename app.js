e3b71a595fae62e67e73b36bb3e2089499fa1e94
pramodkharade <kharade.pramod91@gmail.com>
2019-11-23 15:48:48 +0530

    Error handling if there is no route matched

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const ordersRouter = require('./api/routes/orders');
 app.use(morgan('dev'));
 app.use('/products',productsRoutes);
 app.use('/orders',ordersRouter);
+
 app.use((req,res,next)=>{
-        res.status(200).json({
-            message:'It is working '
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+app.use((errors,req,res,next)=>{
+        res.status(errors.status || 500).json({
+            message: errors.message
         });
 });
 
