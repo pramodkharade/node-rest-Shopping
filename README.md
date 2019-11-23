27af9025dbfde88bc5455067fd021245274516ab
pramodkharade <kharade.pramod91@gmail.com>
2019-11-23 15:36:47 +0530

    install nodemon and morgan : morgan is used to log the api

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ REST API  with nodejs , mongodb
 # Install Express 
 
 npm i --save express
+
+# Install Morgan to Log the api request
+
+ npm i --save morgan
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const productsRoutes = require('./api/routes/products');
 const ordersRouter = require('./api/routes/orders');
-
+app.use(morgan('dev'));
 app.use('/products',productsRoutes);
 app.use('/orders',ordersRouter);
 app.use((req,res,next)=>{
